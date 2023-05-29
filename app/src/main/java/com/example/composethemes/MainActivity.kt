8ab6ce69b10489bdbc40e4e96497803a2ac4c45c
begumavcii <begumavci97@gmail.com>
2023-05-29 18:00:26 +0300

    -update

--- app/src/main/java/com/example/composethemes/MainActivity.kt
+++ app/src/main/java/com/example/composethemes/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.composethemes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
@@ -13,7 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
-class MainActivity : AppCompatActivity() {
+class MainActivity :  ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
