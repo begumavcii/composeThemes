e97a4cc8f015e5592810d1e1d2ec6c337c9e2553
begumavcii <begumavci97@gmail.com>
2023-05-29 17:09:23 +0300

    -added  light and dark theme

--- app/src/main/java/com/example/composethemes/MainActivity.kt
+++ app/src/main/java/com/example/composethemes/MainActivity.kt
@@ -1,43 +1,73 @@
 package com.example.composethemes
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composethemes.ui.theme.ComposeThemesTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeThemesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
+           myApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun myApp() {
+    val isDarkTheme = isSystemInDarkTheme()
+
+    MaterialTheme(
+        colors = if (isDarkTheme) DarkThemeColors else LightThemeColors
+    ) {
+        Surface(color = MaterialTheme.colors.background) {
+            AppContent()
+        }
+    }
+}
+
+@Composable
+fun AppContent() {
     Text(
-            text = "Hello $name!",
-            modifier = modifier
+        text = "Jetpack Compose Material Theme Örneği",
+        style = MaterialTheme.typography.h4,
+        color = MaterialTheme.colors.primary
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ComposeThemesTheme {
-        Greeting("Android")
+fun LightPreview() {
+    MaterialTheme(colors = LightThemeColors) {
+        AppContent()
     }
 }
+
+@Preview(showBackground = true)
+@Composable
+fun DarkPreview() {
+    MaterialTheme(colors = DarkThemeColors) {
+        AppContent()
+    }
+}
+
+private val LightThemeColors = lightColors(
+    primary = Color.Blue,
+    secondary = Color.Red
+)
+
+private val DarkThemeColors = darkColors(
+    primary = Color.Cyan,
+    secondary = Color.Yellow
+)
